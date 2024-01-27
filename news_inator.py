570832afaf02f4187976382d1ccfec775e6497ef
David Whipple <david@inators.net>
2024-01-26 19:50:18 -0700

    logging

--- news_inator.py
+++ news_inator.py
@@ -5,6 +5,10 @@ from guizero import App, Text
 from pprint import pprint
 import textwrap
 import webbrowser
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='mylog.log')
 
 storyCounter = -1
 app = App(title='News-inator', width=600, height=400)
@@ -92,7 +96,39 @@ def openURL():
     url = stories[storyCounter]['url']
     webbrowser.open(url)
 
+def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
+    """
+    Check for internet connectivity by trying to establish a socket connection.
+    :param host: Host to connect to (default is Google's public DNS server).
+    :param port: Port to connect to (default is 53, the DNS service port).
+    :param timeout: Connection timeout in seconds.
+    :return: True if the connection is successful, False otherwise.
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.close()
+        return True
+    except socket.error:
+        return False
+def wait_for_internet_connection(interval=5):
+    """
+    Wait for an internet connection, checking periodically.
+    :param interval: Time in seconds between checks.
+    """
+    print("Checking for internet connection...")
+    while not check_internet_connection():
+        print("No internet connection available. Waiting...")
+        time.sleep(interval)
+    print("Internet connection established.")
+
+
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        wait_for_internet_connection()
+        main()
+    except Exception as e:
+        logging.exception("Something happened")
